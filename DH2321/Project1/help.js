c9afb6cc412011dac4df4492d524e03eb78889e1
Victor Kesten <victorkesten@gmail.com>
2017-01-26 15:35:52 +0100

    bi

--- DH2321/Project1/help.js
+++ DH2321/Project1/help.js
@@ -22,24 +22,48 @@ var nullElement =  [//iPhone
 			{axis:"HCI",value:0},
 			{axis:"UX",value:0}				
 		  ];
-var data = [
-		  [//iPhone
-			{axis:"IVIS",value:0},
-			{axis:"Stats",value:0},
-			{axis:"Math",value:0},
-			{axis:"Art",value:0},
-			{axis:"User",value:0},
-			{axis:"Prog",value:0},
-			{axis:"Graphics",value:0},
-			{axis:"HCI",value:0},
-			{axis:"UX",value:0}				
-		  ]
-		];
+// var data = [
+// 			[
+// 		 	 [
+// 				{axis:"IVIS",value:0},
+// 				{axis:"Stats",value:0},
+// 				{axis:"Math",value:0},
+// 				{axis:"Art",value:0},
+// 				{axis:"User",value:0},
+// 				{axis:"Prog",value:0},
+// 				{axis:"Graphics",value:0},
+// 				{axis:"HCI",value:0},
+// 				{axis:"UX",value:0}				
+// 		  	]
+// 		  ]
+// 		];
+var data;
 var statsForEveryone = [];
-var radarRowList = [];
-var radarNameList = [];
-function tableFix(){
+var radarRowList = [[]];
+var radarNameList = [[]];
 
+
+var groupSelected = 0;
+var groupSizes = [0,0,0,0,0,0,0,0,0,0];
+var groupColors = ["#ff7373","#ffc0cb","d3ffce","#e6e6fa","#7fffd4","#ffa500","#b0e0e6","#008080","#00ff00", "#ff00ff"];
+var initChecks;
+
+
+function tableFix(){
+	data = new Array(10);
+	radarRowList = new Array(10);
+	radarNameList = new Array(10);
+	initChecks = new Array(10);
+	for (var t = 0; t < 10; t++) {
+		initChecks[t] = true;
+	  data[t] = new Array(1);
+	  radarNameList[t] = [];
+	  radarRowList[t] = new Array(1);
+	  data[t][0] = nullElement;
+	}
+
+	//console.log(data);
+	//console.log(data[groupSelected]);
 	var str = document.getElementById("tableData").innerHTML.trim();
 	var ret = '';
 	var a = 0;
@@ -49,7 +73,7 @@ function tableFix(){
 		var ind = sta.substring(14); 
 		name = sta.substring(ind.indexOf("<td>")+18, ind.indexOf("</td>")+14);
 
-		ret += "<tr onclick=\"showPerson(\'"+ name +"\', " + (a+1) + ")\">"+sta.substring(4);
+		ret += "<tr id=\"p"+(a+1)+"\" onclick=\"showPerson(\'"+ name +"\', " + (a+1) + ")\">"+sta.substring(4);
 		str = str.substring(str.indexOf("\n")).trim();
 		var statString = [];
 
@@ -71,15 +95,18 @@ function tableFix(){
 	ret+=" <tr onclick=\"showPerson(\'Uruk\', 74)\"><td>74</td><td>Uruk</td><td>5</td><td>8</td><td>7</td><td>7</td><td>9</td><td>9</td><td>8</td><td>7</td><td>7</td></tr>";
 	document.getElementById("tableData").innerHTML = ret;
 	startTheRadar();
-	console.log(document.getElementById("left").offsetWidth);
-	console.log(document.getElementById("middle").offsetWidth);
-	console.log(document.getElementById("right").offsetWidth);
+	//console.log(document.getElementById("left").offsetWidth);
+	//console.log(document.getElementById("middle").offsetWidth);
+	//console.log(document.getElementById("right").offsetWidth);
+	setGroup(1);
+	setCol(1);
 
 }
 
 
+
 function showPerson(name, row){
-	console.log(name, row);
+	//console.log(name, row);
 	var htm = "";
 	var first = document.getElementById("InterestHobby").innerHTML;
 	var a = 0;
@@ -100,9 +127,21 @@ function showPerson(name, row){
 	first = first.substring(8, first.length-8);
 	second = second.substring(8, second.length-6);
 
+	var bA = "initial";
+	var bR = "none";
+	console.log(radarRowList);
+	console.log(row);
+	for(var b = 0; b < 10; b++){
+		for(var t = 0; t < radarRowList[b].length; t++){
+			if(radarRowList[b][t] == row){
+				bR = "initial";
+				bA = "none";
+			}
+		}
+	}
 
 	var dataFormated = "0";
-	document.getElementById("person").innerHTML = "<h1>" + row + " " + name + "</h1>" + first + "<br><br><br>" + second + "	<br><button type=\"button\" onclick=\"addData('" + name +"', "+ row +")\">Click to add!</button>" + " <br><button type=\"button\" onclick=\"removeData("+ row +")\">Click to remove!</button>";
+	document.getElementById("person").innerHTML = "<h1>" + row + " " + name + "</h1>" + first + "<br><br><br>" + second + "	<br><br><button id=\"butA"+ row +"\" style=\"display:"+bA+";\" type=\"button\" onclick=\"addData('" + name +"', "+ row +")\">Click to add!</button>" + " <button id=\"butR"+ row +"\" type=\"button\" style=\"display:"+bR+";\" onclick=\"removeData("+ row +")\">Click to remove!</button>";
 }
 
 function startTheRadar(){
@@ -130,21 +169,50 @@ function startTheRadar(){
 			  color: color
 			};
 			//Call function to draw the Radar chart
-			RadarChart(".radarChart", data, radarChartOptions);
+			RadarChart(".radarChart", data[groupSelected], radarChartOptions);
 }
-var initCheck = true;
+var clickCheck = false;
+var defaultColor = ["","","","","","","","","","",""];
+
+function setGroup(groupNo){
+	document.getElementById("g"+(groupSelected+1)).style.backgroundColor = "";
+	defaultColor[groupSelected] = "";
+	groupSelected = groupNo - 1;
+	defaultColor[groupSelected] = groupColors[groupSelected];
+	clickCheck = true;
+			var legendString = "";
+		for(var i = 0; i < radarNameList[groupSelected].length; i++){
+			legendString += radarNameList[groupSelected][i] + "<br>";
+		}
+		document.getElementById("legend").innerHTML = legendString;
+	startTheRadar();
+}
+
+function setCol(a){
+	document.getElementById("g"+a).style.backgroundColor = groupColors[a-1];
+}
+
+function unsetCol(a){
+	document.getElementById("g"+a).style.backgroundColor = defaultColor[a-1];
+}
+
 
 function addData(name, row){
+	//console.log(groupColors);
+	if(data[groupSelected].length >=8){
+		console.log("Max Number of people in group");
+		return;
+	}
 	for(var i = 0; i < radarNameList.length; i++){
 		if(name == radarNameList[i]){
 			return;
 		}
 	}
 	var tempAr = statsForEveryone[row-1];
-	console.log(tempAr);
-	if(initCheck){
-		data.pop();
-		initCheck = false;
+	//console.log(tempAr);
+	if(initChecks[groupSelected]){
+		data[groupSelected].pop();
+		initChecks[groupSelected] = false;
 	}
 	var a = [//Nokia Smartphone
 						{axis:"IVIS",value:tempAr[0]},
@@ -157,34 +225,79 @@ function addData(name, row){
 						{axis:"HCI",value:tempAr[7]},
 						{axis:"UX",value:tempAr[8]}
 					  ];
-	data.push(a);
-	radarRowList.push(row);
-	radarNameList.push(name);
+					  //console.log("TEsT"+data[groupSelected]);
+
+	data[groupSelected].push(a);
+
+	if(radarNameList[groupSelected] == null){
+		radarNameList[groupSelected][0] = name;
+	} else {
+		radarNameList[groupSelected].push(name);
+	}
+
+	if(radarRowList[groupSelected][0] == 0){
+		radarRowList[groupSelected][0] = row;
+	} else {
+		radarRowList[groupSelected].push(row);
+	}
+	//console.log(radarNameList[groupSelected]);
+	//radarRowList[groupSelected].push(row);
+	//radarNameList[groupSelected].push(name);
 	var legendString = "";
-	for(var i = 0; i < radarNameList.length; i++){
-		legendString += radarNameList[i] + "<br>";
+	for(var i = 0; i < radarNameList[groupSelected].length; i++){
+		legendString += radarNameList[groupSelected][i] + "<br>";
 	}
 	document.getElementById("legend").innerHTML = legendString;
+	document.getElementById("p" + row).style.backgroundColor = groupColors[groupSelected];
 	startTheRadar();
+	document.getElementById("butA" + row).style.display = "none";
+	document.getElementById("butR" + row).style.display = "initial";
+	console.log(radarRowList[groupSelected]);
 }
 
 function removeData(row){
-	var index = radarRowList.indexOf(row);
+	var index = radarRowList[groupSelected].indexOf(row);
 	if(index > -1){
-		data.splice(index, 1);
-		index = radarRowList.indexOf(row);
-		radarRowList.splice(index, 1);
-		radarNameList.splice(index, 1);
-		if(data.length == 0){
-			data.push(nullElement);
-			initCheck = true;
+		// console.log(data[groupSelected]);
+		// console.log(data[groupSelected].length);
+		console.log(index);
+		if(data[groupSelected].length == 1){
+			initChecks[groupSelected] = true;
+		}
+		data[groupSelected].splice(index-1, 1);
+		
+		console.log(data[groupSelected].length);
+
+		index = radarRowList[groupSelected].indexOf(row);
+		// console.log("CHECK");
+		// console.log(radarRowList[groupSelected]);
+		radarRowList[groupSelected].splice(index, 1);
+		radarNameList[groupSelected].splice(index, 1);
+		// console.log(radarRowList[groupSelected]);
+		// console.log("afa");
+		// console.log(data[groupSelected]);
+		if(radarNameList[groupSelected].length == 1){
+			radarNameList[groupSelected][0] = "";
+		}
+		if(radarRowList[groupSelected].length == 0){
+			radarRowList[groupSelected][0] = 0;
+		}
+		// console.log(data[groupSelected].length);
+		// console.log(data[groupSelected]);
+		if(data[groupSelected].length == 0){
+			//console.log(true);
+			data[groupSelected].push(nullElement);
+			initChecks[groupSelected] = true;
 		}
 
-		var legendString = "";
-		for(var i = 0; i < radarNameList.length; i++){
-			legendString += radarNameList[i] + "<br>";
+		var legendString ="";
+		for(var i = 0; i < radarNameList[groupSelected].length; i++){
+			legendString += radarNameList[groupSelected][i] + "<br>";
 		}
 		document.getElementById("legend").innerHTML = legendString;
+		document.getElementById("p" + row).style.backgroundColor = "";
+		document.getElementById("butA" + row).style.display = "initial";
+		document.getElementById("butR" + row).style.display = "none";
 		startTheRadar();
 	}
 }
@@ -210,11 +323,11 @@ function showRadar(){
 }
 
 function clearData(){
-	radarRowList = [];
-	radarNameList = [];
-	data = [];
-	data.push(nullElement);
-	initCheck = true;
+	radarRowList[groupSelected] = [];
+	radarNameList[groupSelected] = [];
+	data[groupSelected] = [];
+	data[groupSelected].push(nullElement);
+	initChecks[groupSelected] = true;
 	document.getElementById("legend").innerHTML = "";
 	startTheRadar();
 
