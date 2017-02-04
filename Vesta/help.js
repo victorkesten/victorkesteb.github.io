40deefbd0037d637f01f7c13dd5525aad57170d7
Victor Kesten <victorkesten@gmail.com>
2017-02-04 20:16:32 +0100

    Updated

---
+++ Vesta/help.js
@@ -0,0 +1,11 @@
+
+
+function setCol(test){
+	// console.log("hey");
+	document.getElementById("g"+test).style.backgroundColor = "blue";
+
+}
+
+function unsetCol(a){
+	document.getElementById("g"+a).style.backgroundColor = "";
+}
